5b01b7ada76653b3fd68c79909fc4529ad026032
murui <thomas@dippel.dk>
2011-01-03 14:27:26 +0100

    Initial import

---
+++ ConvertNykreditExport/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConvertNykreditExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFilename = args[0];
+            var outputFilename = args[1];
+
+            Console.WriteLine("Converting " + inputFilename + " to " + outputFilename);
+
+            using (var sr = new StreamReader(inputFilename))
+            {
+                using (var sw = new StreamWriter(outputFilename))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var fields = line.Split(';');
+                        var date = fields[0].Replace('-', '/');
+                        var text = fields[1];
+                        var amount = fields[2].Replace(",", "");
+                        var controlled = fields[3];
+                        var balance = fields[4].Replace(",", "");
+                        var bankdate = fields[5];
+
+                        sw.WriteLine(date + "," + amount + "," + "\"" + text + "\"");
+                    }
+                }
+            }
+        }
+    }
+}
